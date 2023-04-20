24372af6d894a1d4a593a55021bff8335fa391ed
pratik bhoite <pratiksbhoite1@gmail.com>
2023-04-20 15:42:28 +0530

    first commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,128 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import homeIcon from "../../Assets/10.png";
+import ScrollToTop from "../ScrollToTop/ScrollToTop";
+import Type from "./Type";
+import "./home.css";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+  AiOutlineMail,
+} 
+from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+
+//import Home2 from "./Home2";
+
+
+function Home() {
+  return (
+    <section>
+      <Container fluid className="home-section" id="home">
+        <Container className="home-content">
+          <Row>
+            <Col md={7} className="home-header">
+              <h1 style={{ paddingBottom: 15 }} className="heading">
+                Hi There!{" "}
+                <span className="wave" role="img" aria-labelledby="wave">
+                  👋🏻
+                </span>{" "}
+                I'M
+              </h1>
+
+              <h1 className="heading-name">
+                <strong className="main-name"> Pratik Bhoite</strong>
+              </h1>
+
+              <div style={{ padding: 30 }} className="type">
+                <Type />
+              </div>
+            </Col>
+
+            <Col md={5} style={{ paddingBottom: 20 }}>
+            {<img
+                src={homeIcon}
+                alt="home pic"
+                className="img-fluid"
+                style={{ paddingTop: 40 }}
+              />
+            } 
+              
+            </Col>
+          </Row>
+          <Row>
+          <Col md={12} className="home-about-social" style={{position : "relative"}}>
+            <h1 data-aos="fade-right">
+              <span className="primary-header">CONNECT </span> WITH ME
+            </h1>
+            <p data-aos="fade-left">Feel free to connect with me</p>
+            <ul className="home-about-social-links" data-aos="fade-up">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/Pratiksbhoite1"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                  aria-label="github"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://twitter.com/PBhoite64219"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                  aria-label="twitter"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="mailto:Pratiksbhoite1@gmail.com"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                  aria-label="email"
+                >
+                  <AiOutlineMail />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/pratik-bhoite-839770232"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                  aria-label="linkedin"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://instagram.com/pratiksbhoite1?igshid=YmMyMTA2M2Y="
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                  aria-label="instagram"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+        </Container>
+      </Container>
+      
+      <ScrollToTop />
+    </section>
+  );
+}
+
+export default Home;
